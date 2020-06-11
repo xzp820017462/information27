28543612be62a2920d04db56e834fd87c085332d
xzp <820017462@qq.com>
2020-06-11 21:31:38 +0800

    add config配置文件，让其更好工作

---
+++ config.py
@@ -0,0 +1,17 @@
+from redis import StrictRedis
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI ="mysql+pymysql://root:@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "w/YHy55xaBzqquY/7IccwsDkKFedltkasyDhSVqbRWeDdzAD1Jzm+clB3iGK+Czh"
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    DEBUG = True
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGNER = True
+    #设置是否过期
+    SESSION_PERMANENT = False
+    #设置session过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+    #制定SESSION保存到REDIS
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
--- manager.py
+++ manager.py
@@ -7,29 +7,16 @@ from flask_migrate import Migrate,MigrateCommand
 #可以用来制定session的位置
 from flask_session import Session
 import pymysql
+from config import Config
 
 
-app = Flask(__name__)                    #mysql+pymysql://root:dzd123@localhost/你的数据库名
-class Config(object):
-    app.config['SQLALCHEMY_DATABASE_URI'] ="mysql+pymysql://root:@127.0.0.1:3306/information27"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    SECRET_KEY = "w/YHy55xaBzqquY/7IccwsDkKFedltkasyDhSVqbRWeDdzAD1Jzm+clB3iGK+Czh"
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    DEBUG = True
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True
-    #设置是否过期
-    SESSION_PERMANENT = False
-    #设置session过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-    #制定SESSION保存到REDIS
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+                   #mysql+pymysql://root:dzd123@localhost/你的数据库名
 
 
 
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_sotre = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
