435769ea8606fd071e3d15b7a1508e11ef03401c
xzp <820017462@qq.com>
2020-06-11 22:19:25 +0800

    将app抽取出来并且发现可以使用db.init_app去初始化

--- info/__init__.py
+++ info/__init__.py
@@ -6,13 +6,15 @@ from flask import Flask,session
 from flask_migrate import Manager,Migrate
 from flask_wtf import CSRFProtect
 from config import config
+#在flask里面可以先初始化扩展的对象,然后再调用init_app
+db = SQLAlchemy()
 
-
-app = Flask(__name__)
-app.config.from_object(config['Testing'])
-db = SQLAlchemy(app)
-redis_sotre = StrictRedis(host=config['Testing'].REDIS_HOST,port=config['Testing'].REDIS_PORT)
-CSRFProtect(app)
-Session(app)
-
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config['Testing'])
+    db.init_app(app)
+    redis_sotre = StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    CSRFProtect(app)
+    Session(app)
+    return app
 
--- manager.py
+++ manager.py
@@ -4,14 +4,14 @@ from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 #可以用来制定session的位置
 
-from info import app,db
+from info import create_app,db
 
                    #mysql+pymysql://root:dzd123@localhost/你的数据库名
 
 
 
 
-
+app = create_app('Testing')
 
 manager = Manager(app)
 #将app与db链接起来
