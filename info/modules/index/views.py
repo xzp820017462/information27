180f13cd05b957ccb12ee80593ba6b2c964a0c77
xzp <820017462@qq.com>
2020-06-16 20:25:48 +0800

    add index.html and templates dir

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,9 @@
+from flask import render_template
+
 from . import index_blu
 from info import redis_store
 @index_blu.route('/')
 def index():
-    redis_store.set("name","itheima")
-    redis_store.set("name","itcast")
-    return "index"
+    #redis_store.set("name","itheima")
+    #redis_store.set("name","itcast")
+    return render_template('news/index.html')
